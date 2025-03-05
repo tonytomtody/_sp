dfaac4a93fa0415d5510e4cb7dd4e40dab28b9a1
tonytomtody <tonytomtody@gmail.com>
2025-03-05 17:07:35 +0800

    week3 add

---
+++ week3/01c-asmFib/power2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int power2(int n) {
+   if(n == 0){
+    return 1;
+   }
+   else {
+    return 2 * power2(n-1);
+   }
+}
+
+int main() {
+   printf("power2(3)=%d\n", power2(3));
+}
